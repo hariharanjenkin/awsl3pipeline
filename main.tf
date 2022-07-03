aefb66855031791450624dd1e2ff34f16b56b710
hariharanjenkin <36661965+hariharanjenkin@users.noreply.github.com>
2022-07-03 11:42:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,18 @@ provider "aws" {
 module "Account-Security"{
   source = "./modules/Account-Security"
 }
+  
+module "account-standardizations"{
+  source = "./modules/account-standardizations"
+  PermissionsBoundary = var.PermissionsBoundary
+  AllowUsersToChangePassword = var.AllowUsersToChangePassword
+  HardExpiry = var.HardExpiry
+  MaxPasswordAge = var.MaxPasswordAge
+  MinimumPasswordLength = var.MinimumPasswordLength
+  PasswordReusePrevention = var.PasswordReusePrevention
+  RequireLowercaseCharacters = var.RequireLowercaseCharacters
+  RequireNumbers = var.RequireNumbers
+  RequireSymbols = var.RequireSymbols
+  RequireUppercaseCharacters = var.RequireUppercaseCharacters
+  LogsRetentionInDays = var.LogsRetentionInDays
+}
