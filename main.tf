bf6d62a2b3f4f2345facd03c66cf26764d18c640
hariharanjenkin <36661965+hariharanjenkin@users.noreply.github.com>
2022-07-03 11:29:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,8 @@
 provider "aws" {
-  region  = "us-east-1"
+  region  = var.region
 }
 
 
 module "Account-Security"{
   source = "./modules/Account-Security"
 }
-
-
-#module "account-standardizations"{
-#  source = "./modules/account-standardizations"
-#  PermissionsBoundary = var.PermissionsBoundary
-#  AllowUsersToChangePassword = var.AllowUsersToChangePassword
-#  HardExpiry = var.HardExpiry
-#  MaxPasswordAge = var.MaxPasswordAge
-#  MinimumPasswordLength = var.MinimumPasswordLength
-#  PasswordReusePrevention = var.PasswordReusePrevention
-#  RequireLowercaseCharacters = var.RequireLowercaseCharacters
-#  RequireNumbers = var.RequireNumbers
-#  RequireSymbols = var.RequireSymbols
-#  RequireUppercaseCharacters = var.RequireUppercaseCharacters
-#  LogsRetentionInDays = var.LogsRetentionInDays
-#}
-#
-#
-#
-#module "log-management"{
-#  source = "./modules/log-management"
-#  OperatorEmail = var.OperatorEmail
-#}
