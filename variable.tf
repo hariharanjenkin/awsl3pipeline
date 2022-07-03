79ea29eaaba03f29e81fef16d6b3312e73eec86f
hariharanjenkin <36661965+hariharanjenkin@users.noreply.github.com>
2022-07-03 11:42:56 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,48 @@
 variable "region" {
 	default = "us-east-1"
 }
+
+variable "PermissionsBoundary" {
+	default = ""
+}
+
+
+variable "AllowUsersToChangePassword"{
+	default = "true"
+}
+
+variable "HardExpiry"{
+	default = "false"
+}
+
+variable "MaxPasswordAge"{
+	default = "90"
+}
+ 
+variable "MinimumPasswordLength"{
+	default = "14"
+}
+
+variable "PasswordReusePrevention"{
+	default = "24"
+}
+ 
+variable "RequireLowercaseCharacters"{
+	default = "true"
+}
+
+variable "RequireNumbers"{
+	default = "true"
+}
+ 
+variable "RequireSymbols"{
+	default = "true"
+}
+  
+variable "RequireUppercaseCharacters"{
+	default = "true"
+}
+ 
+variable "LogsRetentionInDays"{
+	default = "14"
+}
